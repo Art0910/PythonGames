136547517ffdd30c7ad079069b6a6c8952495b57
Art0910 <artkul92@gmail.com>
2018-07-12 21:37:09 +0300

    В классе  "Game" написал функции: "new_number", "compare"

--- Bulls-Cows/Bulls-Cows.py
+++ Bulls-Cows/Bulls-Cows.py
@@ -1,3 +1,6 @@
+import random
+
+
 class Game:
     def __init__(self):
         self.number = []
@@ -15,23 +18,38 @@ class Game:
     def new_number(self):
         """
         Генерируем новой число, которое нужно угадать
-        :return:
+        :return: number как список
         """
-        pass
+        nums = [str(i) for i in range(10)]
+        self.number = []
+        while len(self.number) < 4:
+            random_num = random.randrange(len(nums))
+            self.number.append(nums.pop(random_num))
 
     def compare(self):
         """
         Сравнивает угадываемое число с числом, предложенным пользователем
         :return:
         """
-        pass
+        if ''.join(self.number) == ''.join(self.user_number):
+            print ('Победа! Вы справились за {} попыток'.format(self.attempt))
+        else:
+            self.get_bulls_cows()
 
-    def _get_bulls_cows(self):
+
+    def get_bulls_cows(self):
         """
         Определяет наличие быков и коров и обновляет словарь с быками и коровами
         """
         pass
 
+
 if __name__ == '__main__':
     my_game = Game()
     my_game.start_game()
+
+    # Ниже код для тестирования
+
+    my_game.new_number()
+    my_game.user_number = my_game.number
+    my_game.compare()

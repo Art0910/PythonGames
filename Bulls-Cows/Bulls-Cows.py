872a03f2e921308dcf5a5638ee810814084f4973
Art0910 <artkul92@gmail.com>
2018-07-12 22:03:46 +0300

    В классе "Game" написал функции: "get_user_number", "get_bulls_cows". Артибут bulls_cows по-умолчанию сделал как словарь с ключами "коровы" и "быки"

--- Bulls-Cows/Bulls-Cows.py
+++ Bulls-Cows/Bulls-Cows.py
@@ -6,7 +6,7 @@ class Game:
         self.number = []
         self.user_number = []
         self.attempt = 0
-        self.bulls_cows = {}
+        self.bulls_cows = {'коровы': [], 'быки': []}
 
     def start_game(self):
         """
@@ -26,6 +26,14 @@ class Game:
             random_num = random.randrange(len(nums))
             self.number.append(nums.pop(random_num))
 
+    def get_user_number(self):
+        """
+        Ввод числа пользователем и обновление user_number
+        """
+        user_input = input('Введите число (4 знака): ')
+        self.user_number = [i for i in user_input]
+        print(self.user_number)
+
     def compare(self):
         """
         Сравнивает угадываемое число с числом, предложенным пользователем
@@ -36,13 +44,21 @@ class Game:
         else:
             self.get_bulls_cows()
 
-
     def get_bulls_cows(self):
         """
         Определяет наличие быков и коров и обновляет словарь с быками и коровами
         """
-        pass
-
+        for n_pc, i_pc in enumerate(self.number):
+            for n_usr, i_usr in enumerate(self.user_number):
+                if i_pc == i_usr and n_pc == n_usr:  # определение быка
+                    self.bulls_cows['быки'].append(i_usr)
+                    break
+                elif i_pc == i_usr:  # определение коровы
+                    self.bulls_cows['коровы'].append(i_usr)
+                    break
+                else:                # отсутствие совпадений
+                    pass
+        print(self.bulls_cows)
 
 if __name__ == '__main__':
     my_game = Game()
@@ -50,6 +66,16 @@ if __name__ == '__main__':
 
     # Ниже код для тестирования
 
-    my_game.new_number()
-    my_game.user_number = my_game.number
-    my_game.compare()
+    # my_game.new_number()
+    # my_game.user_number = my_game.number
+    # my_game.compare()
+    # my_game.get_user_number()
+
+    my_game.number = [1, 2, 3, 4]
+    my_game.user_number = [3, 2, 6, 4]
+    my_game.get_bulls_cows()
+
+
+
+    # Сделать фичу - выбор длины числа
+    # При старте игры обнулять все хранимые значения (number, user_number, attempts, bulls_cows)
